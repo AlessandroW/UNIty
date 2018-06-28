a503948791416d7775afc97dbc0e71006bd0c275
Alessandro W <a.wollek@snapaddy.com>
2018-06-28 12:22:58 +0200

    fix: relative path

--- autoUNIty.sh
+++ autoUNIty.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/.skripte/UNIty.sh | grep title
+./UNIty.sh | grep title
 date
 echo sleep 29min
 sleep 29m
@@ -9,7 +9,7 @@ do
     ping -q -c5 google.com > /dev/null
     if [ $? -ne 0 ]
     then
-        ~/.skripte/UNIty.sh | grep title # should equal <title>Logged In</title>
+        ./UNIty.sh | grep title # should equal <title>Logged In</title>
         date
         sleep 29m
     fi
